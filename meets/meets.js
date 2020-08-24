c04b71396d0a77f3e7cd04234f0ba375accb55a7
Pedro <pedromxavier@poli.ufrj.br>
2020-08-24 09:59:42 -0300

    <3

---
+++ meets/meets.js
@@ -0,0 +1,45 @@
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
+function buildMeets(path) {
+    loadJSON(path, function( data ) {
+        let element = document.getElementById("workarea");
+        let innerHTML = []
+        for (let i=0; i<data.length; i++) {
+            let json = data[i];
+            innerHTML.push(`
+            <div class="position">
+			  <!--start button, nothing above this is necessary -->
+			  <div class="svg-wrapper">
+				<svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
+				  <rect id="shape" height="40" width="150" />
+				  <div id="text">
+					<a href="${json['meet']}"><span class="spot"></span>Meet</a>
+				  </div>
+				</svg>
+              </div>
+            </div>
+            `)
+        }
+        element.innerHTML = innerHTML.join("");
+    }, function ( error ){
+        console.log(error)
+    });
+
+}

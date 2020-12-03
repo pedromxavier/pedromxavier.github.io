3acbadfcc18aed662ebe4844b6c681fa04dbe984
Pedro <pedromxavier@poli.ufrj.br>
2020-12-03 19:26:59 -0300

    Update

--- meets/meets.js
+++ meets/meets.js
@@ -17,27 +17,49 @@ function loadJSON(path, success, error)
     xhr.send();
 }
 
+function subtemplate( links ) {
+    let html = []
+    for (let i = 0; i<links.length; i++) {
+        html.push(`
+        <div class="svg-wrapper">
+            <a target="_blank" href="${links[i]["link"]}">
+                <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
+                <rect id="shape" height="40" width="150"></rect>
+                </svg><div id="text">
+                    <p><span class="spot"></span>${links[i]["title"]}</p>
+                </div>
+            </a>
+        </div>
+        `);
+    }
+
+    return html.join("");
+}
+
+function template( data ) {
+    let html = []
+    for (let i=0; i<data.length; i++){
+        html.push(`
+        <div class="position">
+            <h1>${data[i]["title"]}</h1>
+            <h2>${data[i]["teacher"]}</h2>
+            ${subtemplate( data[i]["links"] )}
+            <h3>${data[i]["time"]}</h3>
+        </div>
+        `);
+    }
+    return html.join("");
+}
+
 function buildMeets(path) {
     loadJSON(path, function( data ) {
-        let element = document.getElementById("workarea");
-        let innerHTML = [`<div class="position">`]
-        for (let i=0; i<data.length; i++) {
-            let json = data[i];
-            innerHTML.push(`
-			  <div class="svg-wrapper">
-				<svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
-				  <rect id="shape" height="40" width="150" />
-				  <div id="text">
-					<a href=""><span class="spot"></span>Meet 1</a>
-				  </div>
-				</svg>
-              </div>
-            `)
-        }
-        innerHTML.push(`</div>`)
-        element.innerHTML = innerHTML.join("");
+        $('#workarea').html(`
+            <div class="position">
+                <h2>2020.1</h2>
+            </div>
+            ${template( data )}
+        `);
     }, function ( error ){
         console.log(error)
     });
-
 }

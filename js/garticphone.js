df448e0946eef2370f9a2b917a2eb489d9f3f9ec
Pedro <pedromxavier@poli.ufrj.br>
2021-03-21 20:55:57 -0300

    <3

--- js/garticphone.js
+++ js/garticphone.js
@@ -1,54 +1,47 @@
-var BASE_PATH = "https://pedromxavier.github.io/garticphone";
-
-function readTextFile(file) {
-    var text = null;
-    var rawFile = new XMLHttpRequest();
-    console.log(`GET ${BASE_PATH}/${file}`);
-    rawFile.open("GET", `${BASE_PATH}/${file}`, true);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4) {
-            if (rawFile.status === 200 || rawFile.status == 0) {
-                var text = rawFile.responseText;
-                console.log(text);
-            }
-        }
-    }
-    rawFile.send(null);
-    return text;
-}
-
 function timeConverter(UNIX_timestamp) {
     var a = new Date(~~(Number.parseFloat(UNIX_timestamp) * 1000));
-
     var months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var time = `${date} de ${month} de ${year}.`;
-    return time;
+    return `${a.getDate()} de ${months[a.getMonth()]} de ${a.getFullYear()}.`;
 }
 
-function loadPictures(path, index_path, element) {
-    console.log(`Reading ${index_path}.`);
-    let text = readTextFile(index_path);
-    if (text === null) {
+function readPictureIndex() {
+    var rawFile = new XMLHttpRequest();
+    console.log(`${window.location.href}/pictures/pictures.index`)
+    rawFile.open("GET", `${window.location.href}/pictures/pictures.index`, true);
+    rawFile.onreadystatechange = function () {
+        let text = null;
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                text = rawFile.responseText;
+            }
+        }
+        buildPictures(text);
+    }
+    rawFile.send(null);
+}
+
+function loadPictures() {
+    readPictureIndex();
+}
+
+function buildPictures(index_text) {
+    if (index_text === null) {
         console.log('Reading FAILED.')
         return
     }
     console.log('Reading OK.')
 
-    let index = text.split('\n');
-
+    let index = index_text.split('\n');
     let html = [];
     for (let i = 0; i < index.length; i++) {
         let [file_path, file_date] = index[i].split(' ');
-        html.push(loadPicture(`${path}/${file_path}`, timeConverter(file_date)));
+        html.push(loadPicture(`./pictures/${file_path}`, timeConverter(file_date)));
     }
-    element.innerHTML = html.join(`
+    document.getElementById("pictures").innerHTML = html.join(`
     `);
 }
 
-function loadPicture(path, date) {
+function buildPicture(path, date) {
     return `
     <div class="picture">
         <img alt="" src="${path}"></img>

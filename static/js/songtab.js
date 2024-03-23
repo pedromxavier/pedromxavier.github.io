28f91cca48e5df3917654baeaf186ad7eed30580
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2024-03-23 11:41:34 -0300

    Update

---
+++ static/js/songtab.js
@@ -0,0 +1,16 @@
+function renderTab(element) {
+    // let src_lines = element.innerText.split("\n");
+    // let dst_lines = [];
+
+    // src_lines.forEach(
+    //     _line => {
+    //         let line = _line.trim();
+
+    //         dst_lines.push(line);
+    //     }
+    // );
+
+    // element.innerHTML = dst_lines.join(` <br>\n`);
+
+    return;
+}
